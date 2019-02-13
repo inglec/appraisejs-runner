519f5398ec9975252de80d3a502ac92c1cd06529
inglec <inglec@tcd.ie>
2019-02-13 16:16:53 +0000

    Add promiseWait and promisePartition utility functions

--- src/utils/async.js
+++ src/utils/async.js
@@ -4,4 +4,22 @@ const isPromise = func => func.constructor.name === 'Promise';
 
 const promiseMap = (...args) => Promise.all(_.map(...args));
 
-module.exports = { isPromise, promiseMap };
+const promiseWait = (...args) => (
+  Promise.all(
+    _
+      .map(...args)
+      .map(result => (isPromise(result) ? result.catch(error => error) : result)),
+  )
+);
+
+// Split an array of resolved or rejected values into [resolvedArray, rejectedArray].
+const promisePartition = results => (
+  _.partition(results, result => !(result instanceof Error))
+);
+
+module.exports = {
+  isPromise,
+  promiseMap,
+  promisePartition,
+  promiseWait,
+};
