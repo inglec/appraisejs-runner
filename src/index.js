647d64c879bedc04152af78ac1de20854e6a8afb
inglec <inglec@tcd.ie>
2019-03-04 20:58:27 +0000

    Throw error on unset NODE_PATH

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ function main() {
     throw Error('no path specified');
   }
 
+  if (!process.env.NODE_PATH) {
+    throw Error('environment variable NODE_PATH not set');
+  }
+
   benchmarkProject(args.path, process.env.NODE_PATH)
     // eslint-disable-next-line
     .then(() => {
